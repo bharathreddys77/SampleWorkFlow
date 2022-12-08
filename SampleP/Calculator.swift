8a1dada908a17f61ac5de39ace11c701b7c8662a
Bharathkumarreddy-foreflight <Bharathkumarreddy.vankireddy@boeing.com>
2022-12-08 22:40:30 +0530

    - Test cases

---
+++ SampleP/Calculator.swift
@@ -0,0 +1,14 @@
+//
+//  Calculator.swift
+//  SampleP
+//
+//  Created by Vankireddy, BharathkumarReddy on 08/12/22.
+//
+
+import Foundation
+
+class Calculator{
+    public static func getAddition() -> Int {
+        return 2+3
+    }
+}
